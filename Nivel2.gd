82908daab40e84d253a1b4fbf9a7e456ab89c4d7
MauricioFIUBA <mlcabrera@fi.uba.ar>
2020-02-02 11:41:36 -0300

    Ahora en el Nivel 2 se pueden crear multiples enemigos voladores

--- Nivel2.gd
+++ Nivel2.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 onready var fade_in_timer = Timer.new()
 
+func crearDisparo(laser):
+	add_child(laser)
+
+
 func _ready():
 	$ParallaxBackground.modulate_color(Color(0,0,0))
-	
+	var numero = $Node2D.get_child_count()
+	for i in range(numero):
+		$Node2D.get_child(i).connect("dispare", self, "crearDisparo")
 	fade_in_timer.connect("timeout", self, "fade_in")
 	fade_in_timer.wait_time = 0.1
 	add_child(fade_in_timer)
